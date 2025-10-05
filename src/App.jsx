dedfdd283c822dc6033e92d29c0e236a06660736
Aryan030204 <artistaryan18@gmail.com>
2025-10-05 15:04:16 +0530

    refactor: Remove unused HypeSection import from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import HiringForm from "./components/HiringForm";
-import HypeSection from "./components/HypeSection";
 import Waitlist from "./components/Waitlist";
 
 function App() {
